b4afe383e48c3983bd4717ca5207379081cfb1a9
Tom Black <tom.black06007@gmail.com>
2023-12-29 20:45:17 -0800

    Added player to and player functions to the game.

--- src/main.rs
+++ src/main.rs
@@ -66,6 +66,32 @@ enum GameMode {
     Playing,
     End,
 }
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+       Player {
+        x,
+        y,
+        velocity: 0.0,
+       } 
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(
+            0,
+            self.y,
+            YELLOW,
+            BLACK,
+            to_cp437('@')
+        );
+    }
+}
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
     .with_title("Flappy Dragon")
