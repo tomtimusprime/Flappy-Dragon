1bd5c84e257948e179729544f3af6b67a0d74611
Tom Black <tom.black06007@gmail.com>
2024-01-09 21:02:58 -0800

    added gravity_and_move functionality.

--- src/main.rs
+++ src/main.rs
@@ -89,13 +89,23 @@ impl Player {
             YELLOW,
             BLACK,
             to_cp437('@')
-        );
+        )
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
     }
 }
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
     .with_title("Flappy Dragon")
     .build()?;
-
     main_loop(context, State::new())
 }
